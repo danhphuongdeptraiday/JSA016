3e092485fbc33b6c9b60b703151172f844db388f
Nguyễn Danh Phương <nguyendanhphuong0305@gmail.com>
2022-12-25 10:18:45 +0700

    new commit

--- Day6/app.js
+++ Day6/app.js
@@ -1,5 +1,52 @@
-let inputColor = document.querySelector("input");
-document.querySelector("button").addEventListener("click", () => {
-  document.querySelector(".box").style.backgroundColor = inputColor.value;
-  inputColor.value = "";
-});
+// let inputColor = document.querySelector("input");
+// document.querySelector("button").addEventListener("click", () => {
+//   document.querySelector(".box").style.backgroundColor = inputColor.value;
+//   inputColor.value = "";
+// });
+
+// let person1 = {
+//   name: "Phuong",
+//   password: "123",
+// };
+
+// let t = JSON.stringify(person1);
+// localStorage.setItem("p1", t);
+// let s = JSON.parse(localStorage.getItem("p1"));
+
+// console.log(s);
+
+// console.log(typeof t);
+
+// let person2 = {
+//   name: "Tien",
+//   password: "non",
+// };
+
+// let arrayObject = [];
+// arrayObject.push(person1);
+// arrayObject.push(person2);
+// console.log(arrayObject);
+
+// localStorage.setItem("users", arrayObject);
+
+let uname = document.getElementById("username");
+let pword = document.getElementById("password");
+
+let checkValid = localStorage.getItem("personAccount");
+if (checkValid == null) {
+  localStorage.setItem("personAccount", JSON.stringify([]));
+} else {
+  let listUser = JSON.parse(localStorage.getItem("personAccount"));
+
+  document.querySelector("button").addEventListener("click", function () {
+    listUser.push({
+      username: uname.value,
+      password: pword.value,
+    });
+
+    localStorage.setItem("personAccount", JSON.stringify(listUser));
+
+    uname.value = "";
+    pword.value = "";
+  });
+}
