10a7082180e71a4fa95f2076c3417e9bde12b840
Nguyễn Danh Phương <nguyendanhphuong0305@gmail.com>
2023-01-08 09:09:50 +0700

    update register

--- Day6/form/app.js
+++ Day6/form/app.js
@@ -23,6 +23,7 @@ let list_person_object = [];
 let t = JSON.parse(localStorage.getItem("listUser"));
 if (t == null) {
   localStorage.setItem("listUser", JSON.stringify(list_person_object));
+  location.reload();
 } else {
   let list = JSON.parse(localStorage.getItem("listUser"));
 
@@ -32,17 +33,52 @@ if (t == null) {
     let usernameInput = document.getElementById("username").value;
     let passwordInput = document.getElementById("password").value;
 
-    newUser = {
-      username: usernameInput,
-      password: passwordInput,
-    };
+    // Check user and password
+    let checkUsername = checkValidateUser(usernameInput);
+    let checkPassword = checkPassword1(passwordInput);
 
-    list.push(newUser);
-    localStorage.setItem("listUser", JSON.stringify(list));
+    if (checkUsername == true && checkPassword == true) {
+      console.log("Dkjsdfkn");
+      newUser = {
+        username: usernameInput,
+        password: passwordInput,
+      };
 
-    document.getElementById("username").value = "";
-    document.getElementById("password").value = "";
+      list.push(newUser);
+      localStorage.setItem("listUser", JSON.stringify(list));
+
+      document.getElementById("username").value = "";
+      document.getElementById("password").value = "";
+      alert("Create successfully");
+    }
   });
 }
 
-// localStorage.clear();
+// checkUsername
+function checkValidateUser(userAccount) {
+  if (userAccount == "") {
+    alert("User empty");
+    return false;
+  }
+  // userAccount tên user ng dùng nhập => string
+
+  let listUser = JSON.parse(localStorage.getItem("listUser"));
+  for (let i = 0; i < listUser.length; i++) {
+    if (userAccount == listUser[i].username) {
+      alert("User existed");
+      return false;
+    }
+  }
+
+  return true;
+}
+
+// checkPassword
+function checkPassword1(userPassword) {
+  if (userPassword == "") {
+    alert("password empty");
+    return false;
+  }
+
+  return true;
+}
